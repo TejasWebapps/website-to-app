080c2782c3c07b5977f2c5352c265036975e48fb
TejasWebapps <162024555+TejasWebapps@users.noreply.github.com>
2024-05-18 14:40:55 +0530

    Update MainActivity.java

--- app/src/main/java/com/myexampoint/webtoapp/MainActivity.java
+++ app/src/main/java/com/myexampoint/webtoapp/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     String ShowOrHideWebViewInitialUse = "show";
     private WebView webview ;
     private ProgressBar spinner;
-    String myurl = "https://successtar.github.io"; //Change this  to your website hostname
+    String myurl = "https://tejaswebapps.github.io/GeneralElectionamethi1504/"; //Change this  to your website hostname
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
